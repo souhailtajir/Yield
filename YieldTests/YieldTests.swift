70e26b81fe98c0666d675f4172b9f75f0d6bd81d
Souhail <124731673+souhailtajir@users.noreply.github.com>
2026-01-08 13:50:27 +0100

    Initial Commit

---
+++ YieldTests/YieldTests.swift
@@ -0,0 +1,17 @@
+//
+//  YieldTests.swift
+//  YieldTests
+//
+//  Created by souhail on 1/8/26.
+//
+
+import Testing
+@testable import Yield
+
+struct YieldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
